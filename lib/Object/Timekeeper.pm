ad84d2c68200e707741384a18f0ecf81da4fb3f8
bonnu <s.ohkubo@gmail.com>
2012-11-29 16:51:41 +0900

    initial import

---
+++ lib/Object/Timekeeper.pm
@@ -0,0 +1,41 @@
+package Object::Timekeeper;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Object::Timekeeper - ...
+
+=head1 SYNOPSIS
+
+  use Object::Timekeeper;
+
+=head1 DESCRIPTION
+
+Object::Timekeeper is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Satoshi Ohkubo E<lt>s.ohkubo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Satoshi Ohkubo
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,5 @@
+use strict;
+use Test::More;
+
+BEGIN { use_ok 'Object::Timekeeper' }
+done_testing;
